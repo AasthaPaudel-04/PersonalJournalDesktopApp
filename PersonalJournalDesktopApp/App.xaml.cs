56763fb9c4f49c4d10827de9c866c0bed5aeea5a
Aastha Paudel <paudelaastha5@gmail.com>
2025-12-19 19:47:14 +0545

    Add project files.

---
+++ PersonalJournalDesktopApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalJournalDesktopApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
